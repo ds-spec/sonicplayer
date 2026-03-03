a9252331f3cb3cc509d221f34abb0bc60717982c
Deepak <ds262969@gmail.com>
2026-03-03 23:18:42 +0530

    feat: integrate Tailwind CSS, refactor App component and added notch

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-[#FAFAF9] w-full h-screen font-sans flex justify-center overflow-clip">
+      <div style={{ filter: "drop-shadow(0 3px 12px rgba(0,0,0,0.2))" }}>
+        <div className="flex justify-center items-center w-sm h-16 bg-[#F9F9F9] drop-shadow-2xl notch">
+          <h1 className="text-lg uppercase font-sans leading-none">
+            SONIC BOOM
+          </h1>
+        </div>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+    </div>
   );
 }
 
